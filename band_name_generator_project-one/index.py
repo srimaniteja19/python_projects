0cde8e2e3d8a5a95db6c97d5dd4dbd2613533489
Maniteja <sreemanitejateja@gmail.com>
2021-03-22 19:50:04 +0530

    add first project band_name_generator

---
+++ band_name_generator_project-one/index.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band name generator\n")
+
+city_name = input("What is Your City Name?\n")
+pet_name = input("What is your pet name\n")
+
+band_name = f'Your band name would be {city_name} {pet_name}'
+
+print(band_name)
